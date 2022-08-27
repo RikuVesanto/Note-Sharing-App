0fb37bacdd423da1b9d1613ba1e12ca2bddd87fb
RikuVesanto <riku.vesanto@gmail.com>
2022-08-27 18:50:20 +0300

    [feature] register form and language select

---
+++ src/utils/secure-store.js
@@ -0,0 +1,14 @@
+import * as SecureStore from 'expo-secure-store'
+
+const save = async (key, value) => {
+  await SecureStore.setItemAsync(key, value)
+}
+
+const getValueFor = async (key) => {
+  return await SecureStore.getItemAsync(key)
+}
+
+export default {
+  save: save,
+  getValueFor: getValueFor,
+}
---
+++ src/utils/validation-schemas.js
@@ -0,0 +1,24 @@
+import * as Yup from 'yup'
+import i18n from '../features/language-select/i18n'
+
+module.exports = {
+  LoginValidationSchema: Yup.object().shape({
+    email: Yup.string().required(i18n.t('email_validation')),
+    password: Yup.string().required(i18n.t('password_validation')),
+  }),
+  RegisterValidationSchema: Yup.object().shape({
+    email: Yup.string()
+      .email(i18n.t('register_email_validation'))
+      .required(i18n.t('email_validation')),
+    password: Yup.string()
+      .min(8, i18n.t('register_password_validation_short'))
+      .max(256, i18n.t('register_password_validation_long'))
+      .required(i18n.t('password_validation')),
+    username: Yup.string()
+      .min(4, i18n.t('register_password_validation_short'))
+      .max(32, i18n.t('register_password_validation_long'))
+      .required(i18n.t('password_validation')),
+    name: Yup.string(),
+    school: Yup.string(),
+  }),
+}
